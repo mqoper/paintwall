8eb3a103b42d01b06b42696f19c74aecb6519925
mqoper <mqoper@gmail.com>
2018-12-30 16:31:29 +0100

    Mniejsze poprawki wyglądu i tłumaczenia

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require turbolinks
 
+//= require jquery_ujs
 //= require bootstrap
 //= require masonry/jquery.masonry
 //= require_tree .
+
